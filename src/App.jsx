63cd4b5a39814cb3316d5945e3da12a6392a7b06
DeadCard6 <jp715224@gmail.com>
2026-09-18 11:19:57 -0500

    chore: project setup and MVC architecture structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Evaluación Técnica - The Factory HKA</h1>
+      <p>Proyecto React base listo con arquitectura MVC.</p>
+    </div>
+  );
+}
+
+export default App;
